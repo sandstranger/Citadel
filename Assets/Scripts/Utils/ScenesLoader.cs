fa07425a74399475743452efeaaaffaac1fc202c
sandstranger <sandstranger@gmail.com>
2025-08-23 01:02:10 +0400

    fix levels loading

--- Assets/Scripts/Utils/ScenesLoader.cs
+++ Assets/Scripts/Utils/ScenesLoader.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Citadel.Extensions;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Citadel.SceneManagement
@@ -10,19 +11,25 @@ namespace Citadel.SceneManagement
         public static event Action <Scene> OnStartLoadScene;
         public static event Action<Scene> OnSceneLoaded;
 
-        private static readonly Lazy<List<string>> _levelScenesNames = new(() =>
+        public const string DynamicLevelsSceneName = "CitadelScene";
+        
+        private static readonly List<string> _levelScenesNames = new()
         {
-            var scenesNames = new List<string>();
-            for (var i = 0; i < SceneManager.sceneCountInBuildSettings; ++i)
-            {
-                var scene = SceneManager.GetSceneByBuildIndex(i);
-                if (scene.name.Contains("LevelScene"))
-                {
-                    scenesNames.Add(scene.name);
-                }
-            }
-            return scenesNames;
-        });
+            "0-ReactorLevelScene",
+            "1-MedicalLevelScene",
+            "2-ScienceLevelScene",
+            "3-MaintenanceLevelScene",
+            "4-StorageLevelScene",
+            "5-FlightDeckLevelScene",
+            "6-ExecutiveLevelScene",
+            "7-EngineeringLevelScene",
+            "8-SecurityLevelScene",
+            "9-BridgeLevelScene",
+            "10-AlphaGroveLevelScene",
+            "11-BetaGroveLevelScene",
+            "12-DeltaGroveLevelScene",
+            "13-CyberspaceLevelScene"
+        };
 
         public static async void LoadScene(string sceneName)
         {
@@ -34,9 +41,9 @@ namespace Citadel.SceneManagement
 
         public static void LoadLevel(int level)
         {
-            if (level < _levelScenesNames.Value.Count)
+            if (level < _levelScenesNames.Count)
             {
-                LoadScene(_levelScenesNames.Value[level]);
+                LoadScene(_levelScenesNames[level]);
             }
         }
     }
